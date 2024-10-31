280708d1724d5bd7254b9ee85a9aa30ed9f39fc4
Jeremy Rummel <42078577+jfrummel@users.noreply.github.com>
2024-10-31 18:56:07 -0500

    Update app.py
    Sidebar added with inputs

--- app.py
+++ app.py
@@ -5,13 +5,27 @@ import palmerpenguins
 
 penguins_df = palmerpenguins.load_penguins()
 
+with ui.sidebar(position="left", open="open"):
+    ui.h2("Sidebar")
+    ui.input_selectize(
+        "selected_attribute",
+        "Attribute",
+        choices=["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
+    )
+    ui.input_numeric("plotly_bin_count", "Plotly Count", value=2)
+    ui.input_slider("seaborn_bin_count", "Seaborn Bins", 1, 100, 50)
+    ui.input_checkbox_group(
+        "selected_species_list",
+        "Species",
+        choices=["Adelie", "Gentoo", "Chinstrap"],
+        selected="Adelie",
+        inline=False,
+    )
+    ui.hr()
+    ui.a("Jeremy's GitHub", href="https://github.com/jfrummel/cintel-02-data", target= "_blank")
 ui.page_opts(title="Jeremy's Penguin Histogram", fillable=True)
 with ui.layout_columns():
 
     @render_plotly
     def plot1():
         return px.histogram(px.data.tips(), y="tip")
-
-    @render_plotly
-    def plot2():
-        return px.histogram(px.data.tips(), y="total_bill")
